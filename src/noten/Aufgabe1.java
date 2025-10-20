24b08d2c99529655325f786475a2c3aa38aec1e6
Andreea Badiu <andreea.badiu@stud.ubbcluj.ro>
2025-10-20 22:25:49 +0300

    "Projektstruktur und Aufgabe1"

--- src/noten/Aufgabe1.java
+++ src/noten/Aufgabe1.java
@@ -1,4 +1,12 @@
 package noten;
 
+import java.util.Arrays;
+
 public class Aufgabe1 {
+
+    public static int[] getNichtAusreichendeNoten(int[] noten) {
+        return Arrays.stream(noten)
+                .filter(n -> n < 40)
+                .toArray();
+    }
 }
